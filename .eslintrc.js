a059f3e73192f4842e3cad6ba23b4fbcd197642f
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-11-08 21:03:22 -0300

    Finish config of eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  "env": {
-    "es2021": true,
-    "node": true
-  },
-  "extends": ["eslint:recommended", "airbnb-base", "prettier"],
-  "plugins": ["prettier"],
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
-  },
-  "rules": {
-    "prettier/prettier": "error",
-    "class-methods-use-this":"off",
-    "no-param-reassign": "off",
-    "camelcase": "on",
-    "no-unused-vars": ["error",{ "argsIgnorePattern": "next" }]
-  }
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 };
