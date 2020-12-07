f713e0699321fc56063615278cac5cca65d5e106
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-12-07 19:08:08 -0300

    Optimize of compile tsc in mode dev and create color command

---
+++ src/bot/commands/utilities/color.ts
@@ -0,0 +1,90 @@
+import * as db from 'quick.db'
+import { Command } from "discord-akairo"
+import { Message } from "discord.js"
+import { BotClientTypes } from "../../index";
+
+class ColorCommand extends Command {
+    client: BotClientTypes
+
+    constructor() {
+        super('color', {
+            aliases: ['color'],
+            category: "🛠️ Utilitários | utils",
+            description: {
+                content: "Com esse comando você pode usar uma cor escolhida.",
+                metadata: "Comando para setar color; escolher; buscar cores; cor",
+                usage: "[command] [Cor]",
+                examples: [
+                    "[command] Vermelho",
+                    "[command] 🎨 Vermelho",
+                    "[command] 782371007318196254",
+                    "[command] @🎨 Vermelho"
+                ],
+            },
+            args: [
+                {
+                    id: "color",
+                    type: "string"
+                }
+            ]
+        })
+    }
+
+    async exec(message: Message, { color }: { color: string }) {
+
+        let colorStatus = db.get(`${message.guild.id}.active_colors`) || 'off'
+
+        if (colorStatus.toLowerCase() === "off")
+            return message.util.reply(
+                'O sistema de cores está desabilitado nesse servidor.'
+            )
+
+        const guildRoles = await message.guild.roles.fetch()
+        const roleColor = guildRoles.cache.find((roleCached) => {
+            return (roleCached.name.split(' ')[0] === "🎨"
+                && (roleCached.id === color
+                    || roleCached.name === color
+                    || roleCached.name.split(' ')[1] === color
+                    || !!message.mentions.roles.get(roleCached.id)))
+        })
+
+        const prefix = db.get(`${message.guild.id}.prefix`) || process.env.PREFIX
+
+        console.log(this.prefix)
+
+        if (!roleColor)
+            return message.util.reply(
+                `A cor escolhida não existe, use **\`${prefix}lc\`** para listar todas cores disponíveis.`
+            )
+
+        const meMember = message.guild.me
+
+        if (!meMember.hasPermission('MANAGE_ROLES') || roleColor.position >= meMember.roles.highest.position)
+            return message.util.reply(
+                'Não tenho permissão para dar um cargo de cor para você.'
+            )
+
+        try {
+            
+            const rolesColors = message.member.roles.cache.filter(
+                (role) => (role.name.split(' ')[0] === "🎨")
+            )
+
+            await message.member.roles.remove(rolesColors)
+            await message.member.roles.add(roleColor)
+
+            message.util.reply(rolesColors.size === 0
+                ? `Você trocou ${rolesColors.size > 1 ? 'as cores' : 'a cor'} ${rolesColors.array().join(', ')} para cor \`${roleColor.name}\` com sucesso!`
+                : `Você obteve a cor \`${roleColor.name}\` com sucesso!`)
+
+        } catch (error) {
+
+            message.util.reply('Houve um erro ao obter a cor escolhida')
+
+        }
+
+    }
+
+}
+
+export default ColorCommand
