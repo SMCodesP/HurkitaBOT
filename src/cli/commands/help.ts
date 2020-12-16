01e1b31349f747ed98ccac4c8f7eba26bc9786f0
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-12-16 19:23:57 -0300

    Create paginate of help in cli

--- src/cli/commands/help.ts
+++ src/cli/commands/help.ts
@@ -1,6 +1,7 @@
 import { CommandCLI } from "../structures/entities/CommandCLI";
 import { bgGreen, bgWhite, black, bold, white, yellow } from "colors/safe";
 import CLI from "..";
+import paginate from '../../utils/paginate'
 
 class CommandHelp extends CommandCLI {
     constructor() {
@@ -14,15 +15,24 @@ class CommandHelp extends CommandCLI {
         })
     }
 
-    exec(_: string[], client: CLI, commandUsage: string) {
-        console.log(black(bgGreen(`Lista de comandos disponíveis`)))
+    exec(args: string[], client: CLI, commandUsage: string) {
+        const page = Number(args[0]) || 1
+        const commands: Array<Array<CommandCLI>> = paginate(client.commandHandler.commands, 5)
+        console.log(black(bgGreen(`Lista de comandos disponíveis | ${page}/${commands.length}`)))
         console.log("")
         console.log(`Use ${black(bgWhite(`${commandUsage} {page/comando}`))} para alterar a página ou para obter mais detalhes de um comando.`)
         console.log("")
-        client.commandHandler.commands.forEach((command) => {
+
+        if (!commands[page-1] || commands[page-1].length === 0) {
+            console.log(`Nenhum comando encontrado na página especificada.`)
+            return;
+        }
+
+        commands[page-1].forEach((command: CommandCLI) => {
             console.log(yellow(`${command.id}: `) + white(bold(command.options.description.content)))
         })
     }
+    
 }
 
 export default CommandHelp
