37f3d34b714c58af91c583b22c51fe5ab7d6dbb4
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-12-16 15:16:37 -0300

    Details of log command finish

--- src/bot/listeners/commandFinish.ts
+++ src/bot/listeners/commandFinish.ts
@@ -1,20 +1,36 @@
-import { Message } from 'discord.js';
+import { Message } from "discord.js";
 import { Command, Listener } from "discord-akairo";
-import { blue, red,  } from 'colors/safe';
+import { red, gray } from "colors/safe";
+import * as randomColor from "randomcolor";
+import { hex } from "chalk";
+import * as db from "quick.db";
 
 class CommandFinishListener extends Listener {
 
     constructor() {
-        super('commandFinished', {
-            emitter: 'commandHandler',
-            event: 'commandFinished'
+        super("commandFinished", {
+            emitter: "commandHandler",
+            event: "commandFinished"
         });
     }
 
-    exec(message: Message, command: Command) {
-        console.log(message.member.roles.highest.hexColor)
+    exec(message: Message, command: Command, args: { text: any | any[] }) {
+        const {text} = args;
+        const textArray = text.split(' ')
+        const colorHex = message.member.roles.highest.hexColor
+        const prefix = db.get(`${message.guild.id}.prefix`) || process.env.PREFIX
+        const arrayArguments = textArray
+            .filter(textArgument => String(textArgument).trim().length !== 0)
+            .map((textArgument) => 
+                (typeof textArgument === "string")
+                    ? `"${textArgument.trim()}"`
+                    : textArgument.trim())
 
-        console.log(`${blue(message.author!.tag)} executou o comando ${red(command.id)}.`);
+        console.log("");
+        console.log(`${hex(colorHex).underline(message.author!.tag)} executou o comando ${red(command.id)}:`);
+        console.log(`${hex(randomColor())('Servidor »')} ${gray(message.guild.name)}`);
+        console.log(`${hex(randomColor())('Prefix »')} ${gray(prefix)}`);
+        console.log(`${hex(randomColor())('Argumentos »')} [${gray(arrayArguments.join(', '))}]`);
     }
 }
 
