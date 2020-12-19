46b6e9ef1419bcfb74c88dfb6447879b023a6a32
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-12-19 10:34:33 -0300

    Create categories of anime images

--- src/bot/commands/nsfw/nanime.ts
+++ src/bot/commands/nsfw/nanime.ts
@@ -1,8 +1,11 @@
 import { Command } from "discord-akairo"
 import { MessageEmbed } from "discord.js";
 import { Message } from "discord.js"
-import * as randomAnime from "random-anime"
+import * as db from "quick.db"
+import axios from "axios"
 import { BotClientTypes } from "../../index";
+
+const nsfwTypes = ["waifu", "neko", "trap", "blowjob"]
 
 class AnimeNSFWCommand extends Command {
     client: BotClientTypes
@@ -14,27 +17,70 @@ class AnimeNSFWCommand extends Command {
             description: {
                 content: "Com esse comando você pode gerar uma imagem nsfw de anime.",
                 metadata: "Comando para gerar waifu; anime;",
-                usage: "[command]",
+                usage: "[command] {waifu/neko/trap/blowjob}",
                 examples: [
+                    "[command] waifu",
+                    "[command] neko",
                     "[command]",
                 ],
             },
+            args: [
+                {
+                    id: "type",
+                    type: "string"
+                }
+            ]
         })
     }
 
-    async exec(message: Message) {
+    async exec(message: Message, { type }: { type: string }) {
         if (message.channel.type !== "text")
             return message.util.reply("Você não pode executar um comando nsfw nesse canal.")
         if (!message.channel.nsfw)
             return message.util.reply("O canal não é um nsfw então você não pode executar esse comando aqui.")
 
-        const nsfw = randomAnime.nsfw();
-        const embed = new MessageEmbed()
-            .setColor("RANDOM")
-            .setDescription(`Clique [aqui](${nsfw}) para baixar a imagem.`)
-            .setImage(nsfw);
+        const prefix = db.get(`${message.guild.id}.prefix`) || process.env.PREFIX
+        if (!type) {
+            type = nsfwTypes[Math.floor(Math.random() * nsfwTypes.length)]
+        }
 
-        message.util.reply(embed);
+        if (type === "list") {
+            const embedTypes = new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle("Lista de tipos de nsfw")
+                .setTimestamp()
+                .setFooter(`Copyright © 2020 - ${this.client.user.username}`, this.client.user.displayAvatarURL())
+            
+            nsfwTypes.forEach((typeNSFW) => {
+                embedTypes.addField(`${typeNSFW}`, `\`\`\`yaml\n${prefix}${this.id} ${typeNSFW}\`\`\``, true)
+            })
+
+            message.util.reply(embedTypes)
+            return;
+        }
+
+        if (!nsfwTypes.includes(type))
+            return message.util.reply(`Esse tipo de nsfw não tenho.\nUse \`${prefix}${this.id} list\` para listar todas categorias.`)
+        
+        
+        try {
+            const image = await axios.get(`https://waifu.pics/api/nsfw/${type}`)
+
+            if (!image.data.url)
+                return message.util.reply(
+                    `Não houve nenhum nsfw encontrado de ${type}.`
+                )
+    
+            const embed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setDescription(`Clique [aqui](${image.data.url}) para baixar o nsfw.`)
+                .setImage(image.data.url);
+    
+            message.util.reply(embed);
+        } catch (error) {
+            console.log(error)
+            message.util.reply("Houve um erro ao buscar o nsfw, tente novamente mais tarde.")
+        }
     }
 
 }
---
+++ src/bot/commands/utilities/anime.ts
@@ -0,0 +1,82 @@
+import { Command } from "discord-akairo"
+import { MessageEmbed } from "discord.js";
+import { Message } from "discord.js"
+import * as db from "quick.db"
+import axios from "axios";
+import { BotClientTypes } from "../../index";
+
+const animeTypes = ["waifu", "neko", "shinobu", "megumin", "bully", "cuddle", "cry", "hug", "awoo", "kiss", "lick", "pat", "smug", "bonk", "yeet", "blush", "smile", "wave", "smile", "wave", "highfive", "handhold", "nom", "bite", "glomp", "kill", "slap", "happy", "wink", "poke", "dance", "cringe", "blush"]
+
+class AnimeCommand extends Command {
+    client: BotClientTypes
+
+    constructor() {
+        super('anime', {
+            aliases: ['anime'],
+            category: "🛠️ Utilitários | utils",
+            description: {
+                content: "Com esse comando você pode ganhar uma imagem de anime.",
+                metadata: "Comando para gerar waifu; anime;",
+                usage: "[command] {tipo/list}",
+                examples: [
+                    "[command] waifu",
+                    "[command] list",
+                ],
+            },
+            args: [
+                {
+                    id: "type",
+                    type: "string"
+                }
+            ]
+        })
+    }
+
+    async exec(message: Message, { type }: { type: string }) {
+        const prefix = db.get(`${message.guild.id}.prefix`) || process.env.PREFIX
+        if (!type) {
+            type = animeTypes[Math.floor(Math.random() * animeTypes.length)]
+        }
+
+        if (type === "list") {
+            const embedTypes = new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle("Lista de tipos de imagens")
+                .setTimestamp()
+                .setFooter(`Copyright © 2020 - ${this.client.user.username}`, this.client.user.displayAvatarURL())
+            
+            animeTypes.forEach((typeAnime) => {
+                embedTypes.addField(`${typeAnime}`, `\`\`\`yaml\n${prefix}${this.id} ${typeAnime}\`\`\``, true)
+            })
+
+            message.util.reply(embedTypes)
+            return;
+        }
+
+        if (!animeTypes.includes(type))
+            return message.util.reply(`Esse tipo de imagem não tenho.\nUse \`${prefix}${this.id} list\` para listar todas categorias.`)
+        
+        
+        try {
+            const image = await axios.get(`https://waifu.pics/api/sfw/${type}`)
+
+            if (!image.data.url)
+                return message.util.reply(
+                    `Não houve nenhuma imagem encontrada de ${type}.`
+                )
+    
+            const embed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setDescription(`Clique [aqui](${image.data.url}) para baixar a imagem.`)
+                .setImage(image.data.url);
+    
+            message.util.reply(embed);
+        } catch (error) {
+            console.log(error)
+            message.util.reply("Houve um erro ao buscar uma imagem, tente novamente mais tarde.")
+        }
+    }
+
+}
+
+export default AnimeCommand
