f9ff1bb67c27c20783b401f1267c0f85719cae73
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-12-22 12:48:32 -0300

    Fix bugs

--- src/utils/customConsolePrefixes.ts
+++ src/utils/customConsolePrefixes.ts
@@ -14,7 +14,7 @@ declare global {
 
 const originalConsoleLog = console.log
 
-console.web = (...thisArguments: string[]) => {
+console.bot = (...thisArguments: string[]) => {
   const args = [];
   args.push(getNowTime())
   args.push(green(`[Bot]`) + " -")
@@ -37,7 +37,7 @@ console.web = (...thisArguments: string[]) => {
   }
 }
 
-console.bot = (...thisArguments: string[]) => {
+console.web = (...thisArguments: string[]) => {
   const args = [];
   args.push(getNowTime())
   args.push(white(`[Web]`) + " -")
