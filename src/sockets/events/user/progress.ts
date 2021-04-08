23fe0b566a8a5b2a3cb12c345f5c0e7bfeb4d6b8
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-04-08 19:04:52 +0000

    Log progress

--- src/sockets/events/user/progress.ts
+++ src/sockets/events/user/progress.ts
@@ -7,6 +7,10 @@ export default class Progress extends Event {
   }
 
   run(socket, data) {
+    console.log("")
+    console.log(`${socket.id} Progress`)
+    console.log(data)
+    console.log("")
     progressController.add(socket.id, data)
   }
 }
--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -11,13 +11,14 @@ class Socket {
     this.io = require('socket.io')(http, {
       transport: ['websocket'],
       cors: {
-        origin: ['https://otakutube.vercel.app', 'http://localhost:3000'],
+        origin: ['https://otakutube.vercel.app', 'http://localhost:3000', 'https://3333-black-shrimp-bp9d7t8g.ws-us03.gitpod.io'],
       },
     })
     this.register()
   }
 
   register() {
+    console.log('register')
     registerEvents(path.resolve(__dirname, 'events'), this.io)
   }
 }
