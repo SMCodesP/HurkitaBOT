7e2b8600c614ca34abb5d4f0272008b8215acc78
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-04-13 04:02:50 +0000

    Try use path and host of fix bug

--- src/bot/commands/animes/online.ts
+++ src/bot/commands/animes/online.ts
@@ -1,7 +1,7 @@
 import { Command } from 'discord-akairo'
 import { Message, MessageEmbed } from 'discord.js'
 import ping from 'node-http-ping'
-import fetch from 'node-fetch'
+import * as https from 'https'
 
 import progressController from '../../../sockets/utils/progressController'
 import { BotClientTypes } from '../../index'
@@ -26,42 +26,52 @@ class AnimeInfoCommand extends Command {
     try {
       const progress = progressController.get()
       const time = await ping(process.env.FRONTEND)
-      const response = await fetch(
-        'https://otakutube.tk/api/search?query='
-      )
-      const animes = await response.json()
+      https.get({
+        host: 'otakutube.tk',
+        path: '/api/search?query='
+      }, (res) => {
+        var str = '';
+        console.log('Response is '+res.statusCode);
 
-      const animesAvailable = animes.filter(
-        (item) => !item.category_name.toLowerCase().includes('animetv')
-      )
+        res.on('data', (chunk) => {
+          str += chunk;
+        });
 
-      const embedAnime = new MessageEmbed()
-        .setColor('RANDOM')
-        .setTitle('ℹ️ __Status do site__')
-        .setDescription('**Informações disponíveis sobre nosso site.**')
-        .addField(
-          '\n> `Espectadores ativos »`',
-          `\`\`\`yaml\n${progress.size}\`\`\``,
-          true
-        )
-        .addField('> `Ping »`', `\`\`\`yaml\n${time}\`\`\`\n`, true)
-        .addField(
-          '> `Animes disponíveis »`',
-          `\`\`\`yaml\n${animesAvailable.length}\`\`\`\n`,
-          false
-        )
-        .addField(
-          '\u200B',
-          '**Assista já seus animes em: https://example.com**'
-        )
-        .setThumbnail(message.guild.iconURL())
-        .setTimestamp()
-        .setFooter(
-          `Copyright © 2020 - ${this.client.user.username}`,
-          this.client.user.displayAvatarURL()
-        )
-
-      message.util.reply(embedAnime)
+         res.on('end', () => {
+            const animes = JSON.parse(str)
+            const animesAvailable = animes.filter(
+              (item) => !item.category_name.toLowerCase().includes('animetv')
+            )
+      
+            const embedAnime = new MessageEmbed()
+              .setColor('RANDOM')
+              .setTitle('ℹ️ __Status do site__')
+              .setDescription('**Informações disponíveis sobre nosso site.**')
+              .addField(
+                '\n> `Espectadores ativos »`',
+                `\`\`\`yaml\n${progress.size}\`\`\``,
+                true
+              )
+              .addField('> `Ping »`', `\`\`\`yaml\n${time}\`\`\`\n`, true)
+              .addField(
+                '> `Animes disponíveis »`',
+                `\`\`\`yaml\n${animesAvailable.length}\`\`\`\n`,
+                false
+              )
+              .addField(
+                '\u200B',
+                '**Assista já seus animes em: https://example.com**'
+              )
+              .setThumbnail(message.guild.iconURL())
+              .setTimestamp()
+              .setFooter(
+                `Copyright © 2020 - ${this.client.user.username}`,
+                this.client.user.displayAvatarURL()
+              )
+      
+            message.util.reply(embedAnime)
+         });
+      })
     } catch (error) {
       console.error(error)
       message.util.reply(
