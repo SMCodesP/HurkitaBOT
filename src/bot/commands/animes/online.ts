152f4ad76573f37872a969f83b333908253801fe
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-04-13 09:30:49 -0300

    Use count route api

--- src/bot/commands/animes/online.ts
+++ src/bot/commands/animes/online.ts
@@ -1,7 +1,7 @@
 import { Command } from 'discord-akairo'
 import { Message, MessageEmbed } from 'discord.js'
 import ping from 'node-http-ping'
-import * as https from 'https'
+import axios from 'axios'
 
 import progressController from '../../../sockets/utils/progressController'
 import { BotClientTypes } from '../../index'
@@ -26,52 +26,35 @@ class AnimeInfoCommand extends Command {
     try {
       const progress = progressController.get()
       const time = await ping(process.env.FRONTEND)
-      https.get({
-        host: 'otakutube.tk',
-        path: '/api/search?query='
-      }, (res) => {
-        var str = '';
-        console.log('Response is '+res.statusCode);
+      const {data: animesAvailable} = await axios.get('https://otakutube.tk/api/count')
 
-        res.on('data', (chunk) => {
-          str += chunk;
-        });
+      const embedAnime = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('ℹ️ __Status do site__')
+        .setDescription('**Informações disponíveis sobre nosso site.**')
+        .addField(
+          '\n> `Espectadores ativos »`',
+          `\`\`\`yaml\n${progress.size}\`\`\``,
+          true
+        )
+        .addField('> `Ping »`', `\`\`\`yaml\n${time}\`\`\`\n`, true)
+        .addField(
+          '> `Animes disponíveis »`',
+          `\`\`\`yaml\n${animesAvailable.available}\`\`\`\n`,
+          false
+        )
+        .addField(
+          '\u200B',
+          '**Assista já seus animes em: https://example.com**'
+        )
+        .setThumbnail(message.guild.iconURL())
+        .setTimestamp()
+        .setFooter(
+          `Copyright © 2020 - ${this.client.user.username}`,
+          this.client.user.displayAvatarURL()
+        )
 
-         res.on('end', () => {
-            const animes = JSON.parse(str)
-            const animesAvailable = animes.filter(
-              (item) => !item.category_name.toLowerCase().includes('animetv')
-            )
-      
-            const embedAnime = new MessageEmbed()
-              .setColor('RANDOM')
-              .setTitle('ℹ️ __Status do site__')
-              .setDescription('**Informações disponíveis sobre nosso site.**')
-              .addField(
-                '\n> `Espectadores ativos »`',
-                `\`\`\`yaml\n${progress.size}\`\`\``,
-                true
-              )
-              .addField('> `Ping »`', `\`\`\`yaml\n${time}\`\`\`\n`, true)
-              .addField(
-                '> `Animes disponíveis »`',
-                `\`\`\`yaml\n${animesAvailable.length}\`\`\`\n`,
-                false
-              )
-              .addField(
-                '\u200B',
-                '**Assista já seus animes em: https://example.com**'
-              )
-              .setThumbnail(message.guild.iconURL())
-              .setTimestamp()
-              .setFooter(
-                `Copyright © 2020 - ${this.client.user.username}`,
-                this.client.user.displayAvatarURL()
-              )
-      
-            message.util.reply(embedAnime)
-         });
-      })
+      message.util.reply(embedAnime)
     } catch (error) {
       console.error(error)
       message.util.reply(
