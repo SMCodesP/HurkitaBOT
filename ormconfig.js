43d0600f63e061c9bb3505a113edcbbe0e5198be
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-05-27 13:05:08 -0300

    Fix folder build in production and enviroment url database

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,38 @@
+const { config } = require('dotenv')
+
+config()
+
 module.exports = {
   type: 'postgres',
-  url: 'postgres://iwdpaoqhcfcxkt:5466660f028bfb0332c29050611ddc7657f65e57534c8a9e2ee4eb3dc8680db6@ec2-107-21-10-179.compute-1.amazonaws.com:5432/df0la40itcogiq',
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
-  entities: ['src/entity/**/*.{ts,js}'],
-  migrations: ['src/migration/**/*.{ts,js}'],
-  subscribers: ['src/subscriber/**/*.{ts,js}'],
+  entities: [
+    process.env.NODE_ENV === 'development'
+      ? 'src/entity/**/*.{ts,js}'
+      : 'dist/entity/**/*.{ts,js}',
+  ],
+  migrations: [
+    process.env.NODE_ENV === 'development'
+      ? 'src/migration/**/*.{ts,js}'
+      : 'dist/migration/**/*.{ts,js}',
+  ],
+  subscribers: [
+    process.env.NODE_ENV === 'development'
+      ? 'src/subscriber/**/*.{ts,js}'
+      : 'dist/subscriber/**/*.{ts,js}',
+  ],
   cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber',
+    entitiesDir:
+      process.env.NODE_ENV === 'development' ? 'src/entity' : 'dist/entity',
+    migrationsDir:
+      process.env.NODE_ENV === 'development'
+        ? 'src/migration'
+        : 'dist/migration',
+    subscribersDir:
+      process.env.NODE_ENV === 'development'
+        ? 'src/subscriber'
+        : 'dist/subscriber',
   },
   ssl: true,
   cache: { duration: 60000 },
