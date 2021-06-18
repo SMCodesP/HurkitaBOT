0de4ad783172546bcc9cc24b4f0fcf4296f44864
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-06-18 19:05:44 -0300

    Fix limit of popular animes and add random is popular

--- src/web/controllers/PopularController.ts
+++ src/web/controllers/PopularController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { Controller } from '../structures/entities/Controller'
 import { getConnection } from 'typeorm'
 import { Anime } from '../../entity/Anime'
+import getRandom from '../../utils/getRandom'
 import api from '../../services/api'
 import Category from '../../entities/Category'
 
@@ -18,16 +19,16 @@ class PopularController extends Controller {
     } = req.query as any
 
     let { data: animesPopular } = await api.get<Category[]>(
-      '/api-animesbr-10.php?populares'
+      '/meuanimetv-40.php?populares'
     )
+
     const connection = getConnection()
 
     return res.json(
       await connection
         .getRepository(Anime)
         .findByIds(
-          animesPopular
-            .slice(0, limit)
+          getRandom(animesPopular, Math.min(limit, animesPopular.length))
             .map((anime) => anime.id),
           {
             cache: true,
